a2221fd157d2d45fedcce220950cdef290e723f9
root <preciousamaechi111@yahoo.com>
2022-03-11 13:26:53 -0800

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
-nclude <stdlib.h>                                                                                                                       
+#include <stdlib.h>                                                                                                                       
+
 #include <stdio.h>                                                                                                                      
 
 /**                                                                                                                                     
