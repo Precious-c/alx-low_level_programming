e6fc2cb9cbc6a09db337c3f0f8094a750fd9223a
precious-c <preciousamaechi111@yahoo.com>
2022-07-15 15:25:08 +0100

    print_list.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,7 +3,24 @@
 
 /**
  * main - check the code
- *
+ *#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to struct to free
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
  * Return: Always 0.
  */
 int main(void)
