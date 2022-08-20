d6c204b1cee233238dfe62f4b53c7a0838509322
precious-c <preciousamaechi111@yahoo.com>
2022-08-20 11:51:43 +0000

    m

---
+++ 0x02-functions_nested_loops/1-alpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+void print_alphabet(void)
+{
+	int k;
+	for (k = 'a' ; k <= 'z' ; k++) 
+	{ 
+		_putchar(k); 
+	} 
+	_putchar('\n'); 
+}
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,6 +8,6 @@ void print_alphabet(void)
 	char k;
 
 	for (k = 'a'; k <= 'z'; k++)
-	_putchar(k);
-	_putchar('\n');
+		_putchar(k);
+		_putchar('\n');
 }
